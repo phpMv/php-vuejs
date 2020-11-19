3d669e1020baf20539af4691e186eeea4e4cb9b1
qgorak <q.gorak@gmail.com>
2020-11-19 17:06:00 +0100

    require codeception and test class

---
+++ tests/VueJSTest.php
@@ -0,0 +1,19 @@
+<?php namespace test;
+
+class VueJSTest extends \Codeception\Test\Unit
+{
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
