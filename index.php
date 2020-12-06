314a8b5b696136ed77b2a68563da216445df4b56
unknown <guillaume.jacopin@sts-sio-caen.info>
2020-12-06 18:08:37 +0100

    Test unit/acceptance

--- index.php
+++ index.php
@@ -10,70 +10,15 @@ require "src/PHPMV/VueJS.php";
   	<meta charset="utf-8">
   	<title>Test</title>
   	<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
-  	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-  	<link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-  	<link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+	<script src="test.js"></script>
   	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 </head>
 <body>
 <div id="app" style="width:80%;margin:50px auto;">
-<v-app>
-<template>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="What's your name ?"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email"
-      label="Give us your E-mail"
-      required
-    ></v-text-field>
-
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="How fun is this form ?"
-      required
-    ></v-select>
-
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
-
-    <v-btn
-      :disabled="!valid"
-      color="primary"
-      class="mr-4"
-      @click="validate"
-    >
-      Validate
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-  </v-form>
-</template>
-<v-app>
+<test hello="Salut thierry"></test>
 </div>
-<?php echo $vue; ?>
+<?php
+echo $vue;
+?>
 </body>
 </html>
---
+++ src/tests/unit/VueJSTest.php
@@ -0,0 +1,119 @@
+<?php
+use PHPMV\VueJS;
+use PHPMV\VueJSComponent;
+use function PHPUnit\Framework\assertEquals;
+
+if (! class_exists('\\VueJS')) {
+
+    class VueJSTest extends \Codeception\Test\Unit{
+        private $vue;
+        private $component;
+        
+        protected function assertEqualsIgnoreNewLines($expected, $actual){
+            $this->assertEquals(\str_replace("'",'"',\preg_replace('/\s+/', '',\trim(\preg_replace('/\R+/', '', $expected)))),\str_replace("'",'"',\preg_replace('/\s+/', '',\trim(\preg_replace('/\R+/', '', $actual)))));
+        }
+        
+        protected function _before(){
+            $this->vue=new VueJS('v-app',true,false);
+        }
+
+        protected function _after(){
+            $this->vue=null;
+        }
+
+        public function testAddData(){
+            $this->vue->addData("testData",[0,1,2]);
+            $script=["data"=>["testData"=>[0,1,2]]];
+            $this->assertEquals($script,$this->vue->getData());
+        }
+        
+        public function testAddDataRaw(){
+            $this->vue->addDataRaw("testData","''");
+            $script=["data"=>["testData"=>"!!%''%!!"]];
+            $this->assertEquals($script,$this->vue->getData());
+        }
+        
+        public function testAddMethod(){
+            $this->vue->addMethod("testMethod","console.log('testMethod')");
+            $script=["methods"=>["testMethod"=>"!!%function(){console.log('testMethod')}%!!"]];
+            $this->assertEquals($script,$this->vue->getMethods());
+        }
+        
+        public function testAddComputed(){
+            $this->vue->addComputed("testComputed","console.log('testComputed')");
+            $this->vue->addComputed("testComputedSet","console.log('testComputed')","var data=v");
+            $script=["computeds"=>["testComputed"=>"!!%function(){console.log('testComputed')}%!!","testComputedSet"=>"!!%function(){console.log('testComputed')}, set: function(v){var data=v}%!!"]];
+            $this->assertEquals($script,$this->vue->getComputeds());
+        }
+        
+        public function testAddHook(){
+            $this->vue->onBeforeMount("alert('The page is created')");
+            $this->vue->onMounted("alert('The page is created')");
+            $this->vue->onBeforeCreate("alert('The page is created')");
+            $this->vue->onCreated("alert('The page is created')");
+            $this->vue->onBeforeUpdate("alert('The page is created')");
+            $this->vue->onUpdated("alert('The page is created')");
+            $this->vue->onUpdatedNextTick("alert('The page is created')");
+            $this->vue->onBeforeDestroy("alert('The page is created')");
+            $this->vue->onDestroyed("alert('The page is created')");
+            $script=[
+                "beforeMount"=>"%!!function(){ alert('The page is created') }!!%",
+                "mounted"=>"%!!function(){ alert('The page is created') }!!%",
+                "beforeCreate"=>"%!!function(){ alert('The page is created') }!!%",
+                "created"=>"%!!function(){ alert('The page is created') }!!%",
+                "beforeUpdate"=>"%!!function(){ alert('The page is created') }!!%",
+                "updated"=>"%!!function(){ this.\$nextTick(function () {alert('The page is created')}) }!!%",
+                "beforeDestroy"=>"%!!function(){ alert('The page is created') }!!%",
+                "destroyed"=>"%!!function(){ alert('The page is created') }!!%"
+            ];
+            $this->assertEquals($script,$this->vue->getHooks());
+        }
+       
+        public function testVueJSGetters(){
+            $this->assertEquals(["el"=>'"v-app"',"vuetify"=>"new Vuetify()"],$this->vue->getConfiguration());
+            $this->assertEquals(false,$this->vue->getUseAxios());
+        }
+        
+        public function testVueJSToString() {
+            $this->vue->addDataRaw("email","''");
+            $this->vue->addData("select",null);
+            $this->vue->addMethod("validate","this.\$refs.form.validate()");
+            $this->vue->addWatcher("name","if(this.name=='MyName'){console.log('watcher succeed')}");
+            $this->vue->addComputed("testComputed","console.log('ok')");
+            $this->vue->onMounted("alert('The page is created');");
+            $script='<script>const app=new Vue({el: "v-app",vuetify: new Vuetify(),data:{"email": "","select": null},
+            methods: {"validate": function(){this.$refs.form.validate()}},
+            watch: {"name": function(){if(this.name=="MyName"){console.log("watcher succeed")}}},
+            computeds: {"testComputed": function(){console.log("ok")}},
+            mounted:  function(){ alert("The page is created"); } })</script>';
+            $this->assertEqualsIgnoreNewLines($script,$this->vue->__toString());
+        }
+        
+        public function testAbstractVueJSGetters(){
+            $this->vue->addData("select",null);
+            $this->vue->addMethod("validate","this.\$refs.form.validate()");
+            $this->vue->addComputed("testComputed","console.log('ok')");
+            $this->vue->addWatcher("name","if(this.name=='MyName'){console.log('watcher succeed')}");
+            $this->assertEquals(["data"=>["select"=>NULL]],$this->vue->getData());
+            $this->assertEquals(["methods"=>["validate"=>"!!%function(){this.\$refs.form.validate()}%!!"]],$this->vue->getMethods());
+            $this->assertEquals(["computeds"=>["testComputed"=>"!!%function(){console.log('ok')}%!!"]],$this->vue->getComputeds());
+            $this->assertEquals(["watch"=>["name"=>"!!%function(){if(this.name=='MyName'){console.log('watcher succeed')}}%!!"]],$this->vue->getWatchers());     
+        }
+        
+        public function testVueJSComponent(){
+        	$template='<form method="post">
+            <input type="text" placeholder="test"/>
+            <input type="submit" value="Send"/>
+            </form>';
+        	file_put_contents("test.html",$template);
+        	$this->component=new VueJSComponent("test");
+            $this->component->addMethod("methodTest","console.log('ok')");
+            $script="Vue.component('test',{props: [],methods: {
+            'methodTest': function(){console.log('ok')}
+            },template: '<form method='post'>  <input type='text' placeholder='test'/>  <input type='submit' value='Send'/>  </form>'})";
+            $this->assertEqualsIgnoreNewLines($script,$this->component->create());
+            unlink("test.js");
+            unlink("test.html");
+        }
+    }
+}
