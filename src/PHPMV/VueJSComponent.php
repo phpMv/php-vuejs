2ed84cf99fbad667f1c948d9b6dc70bbedf8e6e4
unknown <guillaume.jacopin@sts-sio-caen.info>
2020-12-12 01:22:40 +0100

    Global component creation

--- src/PHPMV/VueJSComponent.php
+++ src/PHPMV/VueJSComponent.php
@@ -19,12 +19,27 @@ class VueJSComponent extends AbstractVueJS{
         $this->props["props"]=$name;
     }
     
-    public function create():string {
+    public function create(bool $global=false):string {
         $script="Vue.component('".$this->name."',";
         $script.=JavascriptUtils::arrayToJsObject($this->props + $this->data + $this->methods + $this->computeds + $this->watchers + $this->hooks + $this->template);
         $script=JsUtils::cleanJSONFunctions($script);
         $script.=")";
-        \file_put_contents($this->name.".js", $script);
+        if(!$global){
+            \file_put_contents($this->name.".js",$script);
+        }
+        else{
+            if(file_exists("components.js")){
+                \file_put_contents("components.js",PHP_EOL . $script,FILE_APPEND);
+            }
+            else{
+                \file_put_contents("components.js",$script);
+            }
+            
+        }
         return $script;
+    }
+    
+    public function createGlobal():string{
+        return $this->create(true);   
     }
 }
