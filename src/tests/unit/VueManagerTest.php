f61cf1e1adaceb03d38047d459452745a933a3ec
unknown <guillaume.jacopin@sts-sio-caen.info>
2021-06-03 00:35:13 +0200

    fix tests
    codeception requires test method name to have test prefix

--- src/tests/unit/VueManagerTest.php
+++ src/tests/unit/VueManagerTest.php
@@ -6,12 +6,24 @@ use function PHPUnit\Framework\assertEquals;
 use PHPMV\VueManager;
 use PHPMV\VueJSComponent;
 
-if (!class_exists('\\VueManager')) {
+if (! class_exists('\\VueManager')) {
 
 	class VueManagerTest extends Unit {
+		private ?VueManager $vueManager;
 
-		public function globalTest() {
-			$vueManager = VueManager::getInstance();
+		protected function assertEqualsIgnoreNewLines($expected, $actual){
+			$this->assertEquals(\str_replace("'",'"',\preg_replace('/\s+/', '',\trim(\preg_replace('/\R+/', '', $expected)))),\str_replace("'",'"',\preg_replace('/\s+/', '',\trim(\preg_replace('/\R+/', '', $actual)))));
+		}
+
+		protected function _before() {
+			$this->vueManager = VueManager::getInstance();
+		}
+
+		protected function _after() {
+			VueManager::deleteInstance();
+		}
+
+		public function testInjectedScript() {
 			$template = '<p>{{ message }}</p>';
 
 			$component = new VueJSComponent('component-one');
@@ -59,21 +71,21 @@ if (!class_exists('\\VueManager')) {
 			$vue->onUpdated('console.log("hook");');
 			$vue->onUpdatedNextTick('console.log("hook");');
 
-			$vueManager->setAxios(true);
-			$vueManager->addGlobalObservable("state", ["count" => 0]);
-			$vueManager->addGlobalDirective('focus', ['inserted' => 'el.focus();']);
-			$vueManager->addGlobalFilter('capitalize', "if(!value) return '';value = value.toString();return value.charAt(0).toUpperCase() + value.slice(1);", ["value"]);
-			$vueManager->addGlobalComponent($component);
-			$vueManager->addGlobalMixin($mixinOne);
-			$vueManager->addGlobalExtend($extendOne);
-			$vueManager->importComponent($component);
-			$vueManager->importComponent($componentOne);
-			$vueManager->importMixin($mixinOne);
-			$vueManager->importExtend($extendOne);
-			$vueManager->addVue($vue);
+			$this->vueManager->setAxios(true);
+			$this->vueManager->addGlobalObservable("state", ["count" => 0]);
+			$this->vueManager->addGlobalDirective('focus', ['inserted' => 'el.focus();']);
+			$this->vueManager->addGlobalFilter('capitalize', "if(!value) return '';value = value.toString();return value.charAt(0).toUpperCase() + value.slice(1);", ["value"]);
+			$this->vueManager->addGlobalComponent($component);
+			$this->vueManager->addGlobalMixin($mixinOne);
+			$this->vueManager->addGlobalExtend($extendOne);
+			$this->vueManager->importComponent($component);
+			$this->vueManager->importComponent($componentOne);
+			$this->vueManager->importMixin($mixinOne);
+			$this->vueManager->importExtend($extendOne);
+			$this->vueManager->addVue($vue);
 
 			$script = "Vue.prototype.\$http = axios; const state = Vue.observable({count: 0});Vue.directive('focus',{inserted: function(el,binding,vnode,oldVnode){el.focus();}}); Vue.filter('capitalize',function(value){if(!value) return '';value = value.toString();return value.charAt(0).toUpperCase() + value.slice(1);}); Vue.component('component-one',{inheritAttrs: true, model: { prop: 'checked', event: 'change' }, props: [ 'first', 'second' ], activated: function(){console.log('ok');}, deactivated: function(){console.log('ok');}}); Vue.mixin('mixin-one',{}); Vue.extend('extend-one',{mixins: [ mixin-one ]}); const ComponentOne = {inheritAttrs: true, model: { prop: 'checked', event: 'change' }, props: [ 'first', 'second' ], activated: function(){console.log('ok');}, deactivated: function(){console.log('ok');}}; const ComponentTwo = {extends: ComponentOne, data: { message: 'Hello World !' }, template: '<p>{{ message }}</p>'}; const MixinOne = {}; const ExtendOne = {mixins: [ mixin-one ]}; const app = new Vue({el: '#app', components: { 'component-two': ComponentTwo }, directives: { focus: { deep: true, inserted: function(el,binding,vnode,oldVnode){el.focus();} } }, filters: { capitalize: function(value){if(!value) returnvalue = value.toString();return value.charAt(0).toUpperCase() + value.slice(1);} }, data: { raw: true }, computeds: { fullName: { get: function(){return this.firstName+' '+this.lastName}, set: function(v){this.firstName=v[0];this.lastName=v[1]} } }, watch: { title: function(newTitle,oldTitle){console.log('Title change from '+ oldTitle +' to '+ newTitle)} }, mounted: function(){console.log('hook');}, beforeMount: function(){console.log('hook');}, beforeCreate: function(){console.log('hook');}, created: function(){console.log('hook');}, beforeDestroy: function(){console.log('hook');}, beforeUpdate: function(){console.log('hook');}, destroyed: function(){console.log('hook');}, updated: function(){this.\$nextTick(function () {console.log('hook');})}, methods: { alertUser: function(user){alert('Welcome ' + user);} }});";
-			$this->assertEquals($script, $vueManager->__toString());
+			$this->assertEqualsIgnoreNewLines($script, $this->vueManager->__toString());
 		}
 	}
 }
