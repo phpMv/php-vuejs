1c4e955cd14ce55d560e61399f41a018a546a5ab
unknown <guillaume.jacopin@sts-sio-caen.info>
2021-06-02 20:42:12 +0200

    refactor PHPUtils, adding configuration to VueJSComponent

--- src/PHPMV/VueJSComponent.php
+++ src/PHPMV/VueJSComponent.php
@@ -6,79 +6,87 @@ use PHPMV\js\JavascriptUtils;
 use PHPMV\utils\JsUtils;
 
 class VueJSComponent extends AbstractVueJS {
-    protected string $name;
-    protected array $props;
-    protected array $template;
-    protected array $extends;
-    protected ?string $varName;
+	protected string $name;
+	protected array $configuration;
+	protected array $props;
+	protected array $template;
+	protected array $extends;
+	protected ?string $varName;
 
-    public function __construct(string $name,string $varName = null) {
-        parent::__construct();
-        $this->name = $name;
-        $this->props = [];
-        $this->extends = [];
-        $this->template = [];
-        if(!$varName){
-            $varName = JsUtils::kebabToPascal($name);
-        }
-        $this->varName = $varName;
-    }
+	public function __construct(string $name, string $varName = null) {
+		parent::__construct();
+		$this->name = $name;
+		$this->configuration = [];
+		$this->props = [];
+		$this->extends = [];
+		$this->template = [];
+		if (!$varName) {
+			$varName = JsUtils::kebabToPascal($name);
+		}
+		$this->varName = $varName;
+	}
 
-    public function extends(VueJSComponent $component):void {
-        $this->extends['extends'] = $component->getVarName();
-    }
+	public function extends(VueJSComponent $component): void {
+		$this->extends['extends'] = $component->getVarName();
+	}
 
-    public function generateObject():string {
-        $script = JavascriptUtils::arrayToJsObject($this->props + $this->components + $this->filters + $this->extends + $this->mixins + $this->data + $this->computeds + $this->watchers + $this->hooks + $this->methods + $this->template);
-        $script = JsUtils::cleanJSONFunctions($script);
-        return $script;
-    }
+	public function generateObject(): string {
+		$script = JavascriptUtils::arrayToJsObject($this->components + $this->filters + $this->extends + $this->mixins + $this->configuration + $this->props + $this->data + $this->computeds + $this->watchers + $this->hooks + $this->methods + $this->template);
+		$script = JsUtils::cleanJSONFunctions($script);
+		return $script;
+	}
 
-    public function generateGlobalScript():string {
-        $script = "Vue.component('".$this->name."',";
-        $script .= $this->generateObject();
-        $script .= ");";
-        return $script;
-    }
+	public function generateGlobalScript(): string {
+		$script = "Vue.component('" . $this->name . "',";
+		$script .= $this->generateObject();
+		$script .= ");";
+		return $script;
+	}
 
-    public function generateFile(bool $inVariable = false, bool $global = false):void {
-        $script = ($inVariable) ? JsUtils::declareVariable("const", $this->varName, $this->generateGlobalScript(), false) : $this->generateGlobalScript();
-        if (!$global){
-            \file_put_contents($this->name.".js",$script);
-        }
-        elseif(file_exists("components.js")){
-            \file_put_contents("components.js",PHP_EOL . $script,FILE_APPEND);
-        }
-        else{
-            \file_put_contents("components.js",$script);
-        }
-    }
+	public function generateFile(bool $inVariable = false, bool $global = false): void {
+		$script = ($inVariable) ? JsUtils::declareVariable("const", $this->varName, $this->generateGlobalScript(), false) : $this->generateGlobalScript();
+		if (!$global) {
+			\file_put_contents($this->name . ".js", $script);
+		} elseif (file_exists("components.js")) {
+			\file_put_contents("components.js", PHP_EOL . $script, FILE_APPEND);
+		} else {
+			\file_put_contents("components.js", $script);
+		}
+	}
 
-    public function addProps(string ...$props):void {
-        $this->props["props"] = $props;
-    }
+	public function addProps(string ...$props): void {
+		$this->props["props"] = $props;
+	}
 
-    public function addTemplate(string $template):void {
-        $this->template["template"] = $template;
-    }
+	public function setInheritAttrs(bool $inheritAttrs): void {
+		$this->configuration['inheritAttrs'] = $inheritAttrs;
+	}
 
-    public function importTemplate(string $template):void {
-        $this->template["template"] = "'".\str_replace(["\n","\r","\t"]," ",(\file_get_contents($template.'.html',true))."'");
-    }
+	public function setModel(string $prop, string $event): void {
+		$this->configuration['model'] = "{ prop: '" . $prop . "', event: '" . $event . "' }";
+	}
 
-    public function onActivated(string $body):void {
-        $this->addHook("activated", $body);
-    }
+	public function addTemplate(string $template): void {
+		$this->template["template"] = $template;
+	}
 
-    public function onDeactivated(string $body):void {
-        $this->addHook("deactivated", $body);
-    }
+	public function importTemplate(string $template): void {
+		$this->template["template"] = "'" . \str_replace(["\n", "\r", "\t"], " ", (\file_get_contents($template . '.html', true)) . "'");
+	}
 
-    public function getName():string {
-        return $this->name;
-    }
+	public function onActivated(string $body): void {
+		$this->addHook("activated", $body);
+	}
 
-    public function getVarName():string {
-        return $this->varName;
-    }
+	public function onDeactivated(string $body): void {
+		$this->addHook("deactivated", $body);
+	}
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+	public function getVarName(): string {
+		return $this->varName;
+	}
 }
--- src/PHPMV/utils/PhpUtils.php
+++ src/PHPMV/utils/PhpUtils.php
@@ -4,20 +4,24 @@ namespace PHPMV\utils;
 
 class PhpUtils {
 	static private string $delimiter = '---';
-	static private array $parsedFunctions = [];
+	static private array $parsedJs = [];
+
+	public static function importJsFromFile(string $filename): string {
+		return \file_get_contents($filename . '.js', true);
+	}
 
 	public static function parseFile(string $filename, string $extension): array {
-		$pattern = '/' . self::$delimiter . '(.+?)'. self::$delimiter . '(.+?)' . self::$delimiter . 'end' . self::$delimiter . '/s';
-		$templateString = \file_get_contents($filename . '.' . $extension, true);
+		$pattern = '/' . self::$delimiter . '(.+?)' . self::$delimiter . '(.+?)' . self::$delimiter . 'end' . self::$delimiter . '/s';
+		$templateString = self::importFile($filename, $extension);
 		\preg_match_all($pattern, $templateString, $templateArray);
-		for($i=0;$i<count($templateArray[0]);$i++){
-			self::$parsedFunctions[$templateArray[1][$i]] = $templateArray[2][$i];
+		for ($i = 0; $i < count($templateArray[0]); $i++) {
+			self::$parsedJs[$templateArray[1][$i]] = $templateArray[2][$i];
 		}
 		return $templateArray;
 	}
 
-	public static function getParsedFunctions():array {
-		return self::$parsedFunctions;
+	public static function getParsedJs(string $name): string {
+		return self::$parsedJs[$name];
 	}
 }
 
