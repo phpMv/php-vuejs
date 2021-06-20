c326c36f075b022c237612a1d749ad0169f6dc1b
jcheron <myaddressmail@gmail.com>
2021-06-20 03:34:13 +0200

    force tests

--- src/PHPMV/VueJSComponent.php
+++ src/PHPMV/VueJSComponent.php
@@ -25,11 +25,11 @@ class VueJSComponent extends AbstractVueJS {
 	}
 
 	public function addData(string $name, $value): void {
-		$this->data["data"][$name] = $value;
+		$this->data['data'][$name] = $value;
 	}
 
 	public function addDataRaw(string $name, string $value): void {
-		$this->data["data"][$name] = JavascriptUtils::removeQuotes($value);
+		$this->data['data'][$name] = JavascriptUtils::removeQuotes($value);
 	}
 
 	public function extends(VueJSComponent $component): void {
@@ -38,8 +38,8 @@ class VueJSComponent extends AbstractVueJS {
 
 	public function generateObject(): string {
 		$data = [];
-		if (isset($this->data["data"])) {
-			$data["data"] = JavascriptUtils::generateFunction("return " . JavascriptUtils::arrayToJsObject($this->data["data"]));
+		if (isset($this->data['data'])) {
+			$data['data'] = JavascriptUtils::generateFunction("return " . JavascriptUtils::arrayToJsObject($this->data["data"]));
 		}
 		return JavascriptUtils::arrayToJsObject($this->components + $this->filters + $this->extends + $this->mixins + $this->configuration + $this->props + $data + $this->computeds + $this->watchers + $this->hooks + $this->methods + $this->template);
 	}
@@ -53,15 +53,15 @@ class VueJSComponent extends AbstractVueJS {
 		$script = ($inVariable) ? JavascriptUtils::declareVariable("const", $this->varName, $this->generateGlobalScript(), false) : $this->generateGlobalScript();
 		if (!$global) {
 			\file_put_contents("$this->name.js", $script);
-		} elseif (file_exists("components.js")) {
-			\file_put_contents("components.js", PHP_EOL . $script, FILE_APPEND);
+		} elseif (\file_exists("components.js")) {
+			\file_put_contents("components.js", \PHP_EOL . $script, \FILE_APPEND);
 		} else {
 			\file_put_contents("components.js", $script);
 		}
 	}
 
 	public function addProps(string ...$props): void {
-		$this->props["props"] = $props;
+		$this->props['props'] = $props;
 	}
 
 	public function setInheritAttrs(bool $inheritAttrs): void {
@@ -73,15 +73,15 @@ class VueJSComponent extends AbstractVueJS {
 	}
 
 	public function addTemplate(string $template): void {
-		$this->template["template"] = $template;
+		$this->template['template'] = $template;
 	}
 
 	public function onActivated(string $body): void {
-		$this->addHook("activated", $body);
+		$this->addHook('activated', $body);
 	}
 
 	public function onDeactivated(string $body): void {
-		$this->addHook("deactivated", $body);
+		$this->addHook('deactivated', $body);
 	}
 
 	public function getName(): string {
