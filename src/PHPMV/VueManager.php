d92b486a22bd4943d9ffb00512bfa4a224560cd2
jcheron <myaddressmail@gmail.com>
2021-06-20 17:01:06 +0200

    [skip ci] add createComponent method in VueManager

--- src/PHPMV/VueManager.php
+++ src/PHPMV/VueManager.php
@@ -28,16 +28,16 @@ class VueManager {
 		$this->useAxios = false;
 	}
 
-	protected function getTemplateComponentDirectory(): string{
-		return $this->config['templateDir']??'vuejs/';
+	protected function getTemplateComponentDirectory(): string {
+		return $this->config['templateDir'] ?? 'vuejs/';
 	}
 
-	public static function getInstance(?object $container=null): ?VueManager {
+	public static function getInstance(?object $container = null): ?VueManager {
 		if (!isset(self::$instance)) {
 			self::$instance = new static();
 		}
-		if(isset($container)){
-			self::$instance->container=$container;
+		if (isset($container)) {
+			self::$instance->container = $container;
 		}
 		return self::$instance;
 	}
@@ -47,37 +47,37 @@ class VueManager {
 	}
 
 	public function importComponentObject(VueJSComponent $component): void { //component, mixin, or extend
-		$this->imports[] =$component;
+		$this->imports[] = $component;
 	}
 
 	public function addGlobalDirective(string $name, array $hookFunction) {
 		foreach ($hookFunction as $key => $value) {
 			$hookFunction[$key] = JavascriptUtils::generateFunction($value, ['el', 'binding', 'vnode', 'oldVnode']);
 		}
-		$this->imports[] ="Vue.directive('$name',".JavascriptUtils::arrayToJsObject($hookFunction).");";
+		$this->imports[] = "Vue.directive('$name'," . JavascriptUtils::arrayToJsObject($hookFunction) . ");";
 	}
 
 	public function addGlobalFilter(string $name, string $body, array $params = []): void {
-		$this->imports[] ="Vue.filter('$name',". JavascriptUtils::generateFunction($body, $params, false) .");";
+		$this->imports[] = "Vue.filter('$name'," . JavascriptUtils::generateFunction($body, $params, false) . ");";
 	}
 
 	public function addGlobalObservable(string $varName, array $object): void {
-		$this->imports[] =JavascriptUtils::declareVariable('const', $varName, "Vue.observable(" . JavascriptUtils::arrayToJsObject($object) . ")", false);
+		$this->imports[] = JavascriptUtils::declareVariable('const', $varName, "Vue.observable(" . JavascriptUtils::arrayToJsObject($object) . ")", false);
 	}
 
 	public function addGlobalMixin(VueJSComponent $mixin): void {
 		$mixin->setTypeAndGlobal('mixin');
-		$this->imports[] =$mixin;
+		$this->imports[] = $mixin;
 	}
 
 	public function addGlobalExtend(VueJSComponent $extend): void {
 		$extend->setTypeAndGlobal('extend');
-		$this->imports[] =$extend;
+		$this->imports[] = $extend;
 	}
 
 	public function addGlobalComponent(VueJSComponent $component): void {
 		$component->setGlobal(true);
-		$this->imports[] =$component;
+		$this->imports[] = $component;
 	}
 
 	public function addVue(VueJS $vue): void {
@@ -85,31 +85,44 @@ class VueManager {
 	}
 
 	/**
-	 * Creates and returns a new VueJS instance.
+	 * Creates, adds and returns a new VueJS instance.
 	 *
-	 * @param string $element
-	 * @param string|null $varName
-	 * @param false $useVuetify
-	 * @return VueJS
+	 * @param string $element The dom selector associated with this Vue
+	 * @param string|null $varName The vue variable name
+	 * @param false $useVuetify True if Vuutify is used
+	 * @return VueJS The created vue
 	 */
-	public function createVue(string $element,?string $varName=null,bool $useVuetify=false): VueJS {
-		$config=$this->config;
-		$config['el']=$element;
-		$varName??='app'.(\count($this->vues)+1);
-		return $this->vues[]=new VueJS($config,$varName,$useVuetify);
+	public function createVue(string $element, ?string $varName = null, bool $useVuetify = false): VueJS {
+		$config = $this->config;
+		$config['el'] = $element;
+		$varName ??= 'app' . (\count($this->vues) + 1);
+		return $this->vues[] = new VueJS($config, $varName, $useVuetify);
+	}
+
+	/**
+	 * Creates, adds and returns a new VueJSComponent instance (component, mixin or extend).
+	 * @param string $name The component name
+	 * @param string|null $varName The component varName
+	 * @param string $type The object type : one of component (default value), extend or mixin
+	 * @return VueJSComponent The created component
+	 */
+	public function createComponent(string $name, string $varName = null, string $type='component'): VueJSComponent {
+		$compo=new VueJSComponent($name,$varName,$type);
+		$this->addGlobalComponent($compo);
+		return $compo;
 	}
 
 	public function __toString(): string {
 		$script = '';
-		if ($this->useAxios){
+		if ($this->useAxios) {
 			$script = 'Vue.prototype.$http = axios;' . \PHP_EOL;
 		}
 
-		foreach($this->imports as $import) {
-			$script.=$import.\PHP_EOL;
+		foreach ($this->imports as $import) {
+			$script .= \implode(\PHP_EOL, $import);
 		}
-		foreach($this->vues as $vue) {
-			$script.=$vue.\PHP_EOL;
+		foreach ($this->vues as $vue) {
+			$script .= \implode(\PHP_EOL, $vue);
 		}
 
 		$script = JavascriptUtils::cleanJSONFunctions($script);
@@ -121,15 +134,15 @@ class VueManager {
 	}
 
 	/**
-	 * Sets the global VueJS configuration array with el, delimiters, useAxios.
+	 * Sets the global VueJS configuration array with el, delimiters, useAxios, templateDir.
 	 *
 	 * @param array $config
 	 */
 	public function setConfig(array $config): void {
-		if(isset($config['useAxios'])) {
+		if (isset($config['useAxios'])) {
 			$this->setAxios($config['useAxios']);
 			unset($config['useAxios']);
 		}
-		$this->config=$config;
+		$this->config = $config;
 	}
 }
